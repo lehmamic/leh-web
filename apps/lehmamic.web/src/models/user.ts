37ad03d19416e75d3f12c613675f8ecd4ac29d37
Michael Lemann <2562610+lehmamic@users.noreply.github.com>
2022-05-16 08:44:40 +0200

    Extend teh userproperties

--- apps/lehmamic.web/src/models/user.ts
+++ apps/lehmamic.web/src/models/user.ts
@@ -2,7 +2,9 @@ export const USERS_COLLECTION = "users";
 
 export interface User {
   _id: string;
+  userId: string;
   firstName: string;
   lastName: string;
   email: string;
+  bio?: string;
 }
